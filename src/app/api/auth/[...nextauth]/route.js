4e02e380a143d5686bd9dc0c7a1aae12b9e3a686
Arman Mir <mirarman8583@gmail.com>
2025-12-21 11:12:16 +0600

    Credentials provider setup

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,32 @@
+import NextAuth from "next-auth"
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    CredentialsProvider({
+    // Sign In with {name} Button
+    name: 'Credentials',
+    
+
+    // form inputs
+
+
+
+    credentials: {
+      username: { label: "Username", type: "text", placeholder: "jsmith" },
+      password: { label: "Password", type: "password" }
+    },
+    async authorize(credentials, req) {
+     
+        // my own login logic
+
+
+      return null
+    }
+  })
+    
+    // ...add more providers here
+  ],
+}
+
+export default NextAuth(authOptions)
