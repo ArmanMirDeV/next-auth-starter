80018c35d1e3d6e61c88fe5cdb54e078f9befa2c
Arman Mir <mirarman8583@gmail.com>
2025-12-23 07:35:18 +0600

    Admin Routes setup

---
+++ src/app/forbidden/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ForbiddenPage = () => {
+  return (
+    <div>
+        <h2 className='py-20 text-8xl text-center  '>
+            ForbiddenPage
+        </h2>
+    </div>
+  )
+}
+
+export default ForbiddenPage
